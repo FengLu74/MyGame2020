3a69aabdcf5f95421d5d9bdc87f47d5b1c5b88ed
lh5061671 <Floydxiu5061671>
2020-08-19 14:25:39 +0800

    搭建client架子

---
+++ MyGameClient2020/Assets/Scripts/Battle/LogicWorldElement.cs
@@ -0,0 +1,15 @@
+﻿namespace MGame.GameBattle.Logic
+{
+    /// <summary>
+    /// 战斗单位
+    /// </summary>
+    public abstract class LogicWorldElement
+    {
+        private LogicWorldElement eType;
+        private bool isActive = false;
+        private int instanceId;
+
+    }
+}
+
+
