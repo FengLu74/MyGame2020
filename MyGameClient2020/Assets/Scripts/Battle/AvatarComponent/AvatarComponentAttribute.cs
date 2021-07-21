833db2ee17451a26bd957c5ff09d34c0033a7d2e
= <1053817073@qq.com>
2021-07-21 14:12:00 +0800

    # 201 7-21 继续实现

--- MyGameClient2020/Assets/Scripts/Battle/AvatarComponent/AvatarComponentAttribute.cs
+++ MyGameClient2020/Assets/Scripts/Battle/AvatarComponent/AvatarComponentAttribute.cs
@@ -11,7 +11,17 @@ namespace Battle.Logic
         {
             base.Initialize(pAvatar);
             NumericDic = new Dictionary<int, Fix64>();
+            CreateAttributeDict();
         }
+        private void CreateAttributeDict()
+        {
+            
+        }
+
+
+
+
+
 
         public Fix64 this[NumericType numeric]
         {
@@ -27,7 +37,6 @@ namespace Battle.Logic
             }
         }
 
-
         private Fix64 getValueByKey(int key)
         {
             Fix64 value = Fix64.Zero;
@@ -36,13 +45,22 @@ namespace Battle.Logic
         }
 
 
-
-
-
-
         private void updateAttr(NumericType numericType)
         {
+            if (numericType < NumericType.Max) // 基础数值 不必更新
+            {
+                return;
+            }
+            int final = (int)numericType / 10;
+            int bas = final * 10 + 1;
+            int pct = final * 10 + 2;
+            int add = final * 10 + 3;
 
+            int max = final + 1;
+            //                 基础值                 附加的绝对值               附加的百分比值
+            Fix64 result = getValueByKey(bas) + getValueByKey(add) + getValueByKey(bas) * getValueByKey(pct) / 1000 ;
+            NumericDic[final] = result;
+            //数值变化通知
         }
         public override void Clear()
         {
