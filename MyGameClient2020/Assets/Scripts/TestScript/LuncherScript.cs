2c8ac07969b88f58afee41a06b7f6b534f15b211
= <1053817073@qq.com>
2021-07-27 14:22:19 +0800

    # 2021-7-27 Luncher  到Battle
    1：实现一个战斗，
    2：引入ET
    3：做个匹配，回放

---
+++ MyGameClient2020/Assets/Scripts/TestScript/LuncherScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LuncherScript : MonoBehaviour
+{
+    public InputField field;
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button.ButtonClickedEvent btnEvent = button.onClick;
+        if (btnEvent != null)
+        {
+            btnEvent.RemoveAllListeners();
+            btnEvent.AddListener(
+                () =>
+                {
+                    OnClickEvent();
+                }
+                );
+        }
+    }
+    private void OnClickEvent()
+    {
+        if(field.text!=null)
+        {
+            SceneManager.LoadScene("Battle");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
