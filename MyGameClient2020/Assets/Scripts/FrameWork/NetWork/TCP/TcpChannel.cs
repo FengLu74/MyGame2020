4d2e8cf9779944fa80ee63f568cf29137109187f
= <1053817073@qq.com>
2021-08-23 19:37:29 +0800

    # 2021-8-23 完善 网络模块

---
+++ MyGameClient2020/Assets/Scripts/FrameWork/NetWork/TCP/TcpChannel.cs
@@ -0,0 +1,34 @@
+﻿
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FrameWork.NetWork
+{
+    public class TcpChannel : NetChannel
+    {
+        private Socket socket;
+        private readonly MemoryStream memoryStream;
+
+        public override void Send(MemoryStream stream)
+        {
+
+        }
+        public override MemoryStream Stream
+        {
+            get
+            {
+                return memoryStream;
+            }
+        }
+        public override void Start()
+        {
+
+        }
+        public TcpChannel(IPEndPoint iPEndPoint,NetService service):base(service,ChannelType.Connect)
+        {
+
+        }
+    }
+}
+
